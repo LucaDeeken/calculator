bad6c49557571d0683a2e81bce689965f7c58a5f
LucaDeeken <LDeeken@aol.com>
2024-08-16 16:24:53 +0200

    functions for all of the basic math operators

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// functions for all of the basic math operators 
+
+function add(a, b){
+
+    let sum = a+b;
+
+    return sum;
+
+};
+
+function subtract (a, b) {
+    
+    let sub = a-b;
+
+    return sub;
+
+};
+
+function multiply (a, b) {
+
+    let multi = a*b;
+
+    return multi;
+};
+
+function divide (a, b) {
+
+    let divide = a / b;
+    return divide;
+};
